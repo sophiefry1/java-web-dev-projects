a3dccb35d254e3b6b82c589b0b3a2f82269d3402
Sophie Fry <fryxxsop@icloud.com>
2023-11-30 17:15:37 -0600

    First exercise unfinished

---
+++ datatypes/datatypes-exercises/java.java
@@ -0,0 +1,10 @@
+package org.launchcode;
+import java.util.Scanner;
+Scanner input = new Scanner(System.in);
+System.out.println(Hello, what is your name:);
+String name = input.nextLine();
+System.out.Println("Hello " + name);
+
+
+public class java {
+}
