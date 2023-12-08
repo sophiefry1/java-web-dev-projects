fe200ac07fbce3614b123698dd6a55d94ccbd23c
Sophie Fry <fryxxsop@icloud.com>
2023-12-07 20:01:43 -0600

    unfinished studio

---
+++ classes-part-one/studio/restaurant-menu/src/main/java/org/launchcode/Menu.java
@@ -0,0 +1,37 @@
+package org.launchcode;
+
+import static java.awt.SystemColor.menu;
+
+public class Menu {
+    public void setMenu(String name) {
+        this.menu = menu;
+    }
+
+    public void setprice(int price) {
+        int price1 = price;
+
+    }
+
+    public void setMenuItem(double price) {
+        this.getPrice() = (int) price;
+    }
+
+    private void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public String getMenu() {
+        return name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public int getMenuItem() {
+        int MenuItem;
+        return MenuItem;
+    }
+
+
+}
